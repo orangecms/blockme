7fc44cd88fd6a12836254cf5561c4c82bd48bc12
Daniel Maslowski <daniel.maslowski@shop.co>
2016-08-28 15:06:45 +0200

    ~parametrize loop iteration counts and test for small and large amounts

--- spec/blockmeSpec.js
+++ spec/blockmeSpec.js
@@ -9,7 +9,7 @@ class Timer {
       const timeout = 1000;
       setTimeout(() => {
         const timeTaken = new Date() - startTime;
-        console.info(`\n--- Time taken: ${timeTaken}ms ---\n`);
+        console.info(`\n\n--- Time taken: ${timeTaken}ms ---`);
         (timeTaken < timeout) ? resolve() : reject();
       }, 0);
     });
@@ -21,19 +21,47 @@ class Timer {
 };
 
 describe('blockme', () => {
-  it('should block', (done) => {
-    const timer = new Timer();
+  it('should not block for huge amounts of data', (done) => {
     const blocker = new Blocker();
-    blocker.blockMe();
-    timer.stop().catch(done); // we expect the promise to be broken (timeout)
+    expect(blocker.isDone()).toEqual(false);
+    (new Timer()).stop()
+      .then(() => {
+        // we expect the promise to be kept (no timeout)
+        expect(blocker.isDone()).toEqual(true);
+        done();
+      })
+      .catch(() => expect(blocker.isDone()).toEqual(false));
+    blocker.blockMe(1000); // a thousand
+  });
+
+  it('should block for huge amounts of data', (done) => {
+    const blocker = new Blocker();
+    expect(blocker.isDone()).toEqual(false);
+    (new Timer()).stop()
+      .then(() => expect(blocker.isDone()).toEqual(false))
+      .catch(() => {
+        // we expect the promise to be broken (timeout)
+        expect(blocker.isDone()).toEqual(true);
+        done();
+      });
+    blocker.blockMe(1000 * 1000 * 1000); // a billion
   });
 });
 
 describe('blockmenot', () => {
-  it('should not block', (done) => {
-    const timer = new Timer();
+  it('should not block, even for huge amounts of data', (done) => {
     const blocker = new Blocker();
-    blocker.blockMeNot();
-    timer.stop().then(done); // we expect the promise to be kept (no timeout)
+    expect(blocker.isDone()).toEqual(false);
+    (new Timer()).stop()
+      .then(() => {
+        // we expect the promise to be kept (no timeout)
+        // however, since it's async, `blocker.isDone()` might return false
+        const isDone = blocker.isDone();
+        expect(([true, false]).includes(isDone)).toEqual(true);
+        console.info(`Data processing is ${isDone ? '' : 'not yet '}done.`);
+        done();
+      })
+      .catch(() => expect(blocker.isDone()).toEqual(false));
+    blocker.blockMeNot(1000 * 1000 * 1000); // a billion
   });
 });
